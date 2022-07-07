c8e212e74d9b9e857836f79f1dfad9c4fd89d55f
Andres A <andres@netzstrategen.com>
2022-07-07 12:55:42 +0200

    Refactored code to avoid filter loop.

--- src/ExportPatchesCommand.php
+++ src/ExportPatchesCommand.php
@@ -2,8 +2,6 @@
 
 namespace Makers99\SharedPatches;
 
-use WP_CLI;
-
 /**
  * WP-CLI command for shared patches.
  *
@@ -21,7 +19,7 @@ class ExportPatchesCommand extends \WP_CLI_Command {
     // Return if this is not Flynt based.
     $composer_path = dirname(ABSPATH) . '/../composer.json';
     if (!file_exists($composer_path)) {
-      WP_CLI::error('No root composer.json was found (path was: ' . $composer_path . ').');
+      \WP_CLI::error('No root composer.json was found (path was: ' . $composer_path . ').');
     }
     // Read project root composer.json, to only include relevant patches.
     $composer_json = json_decode(file_get_contents($composer_path), TRUE);
@@ -33,22 +31,22 @@ class ExportPatchesCommand extends \WP_CLI_Command {
       array_keys($composer_json['require'])
     );
 
+    $dependencies = array_combine($dependencies, $dependencies);
+
     $patches = [];
     foreach (glob(dirname(__DIR__) . "/patches/*.patch") as $patch) {
       $patch_parts = explode('.', basename($patch));
-      $plugin = reset($patch_parts);
-      $plugin_name = (array) array_filter($dependencies, fn($dependency) => $dependency === $plugin);
-      $plugin_name = reset($plugin_name);
-      if (!$plugin_name) {
+      $plugin_name = $patch_parts[0];
+      $keywords = $patch_parts[3];
+      if (!isset($dependencies[$plugin_name])) {
         continue;
       }
-      end($patch_parts);
-      $patches[$plugin_name][prev($patch_parts)] = './.wp-cli/packages/shared-patches/patches/' . basename($patch);
+      $patches[$plugin_name][$keywords] = './.wp-cli/packages/shared-patches/patches/' . basename($patch);
     }
     // Map all references to 'patches.json', and then reference this as
     // `patches-file` in the root composer.json.
     if (file_put_contents(dirname(ABSPATH) . '/../patches.json', json_encode(['patches' => $patches], JSON_PRETTY_PRINT))) {
-      WP_CLI::success('Patches successfully mapped at ' . realpath(dirname(ABSPATH) . '/../patches.json'));
+      \WP_CLI::success('Patches successfully mapped at ' . realpath(dirname(ABSPATH) . '/../patches.json'));
     };
   }
 
