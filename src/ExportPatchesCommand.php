0848da9a9a24d88a39e01756f3e5da6797ffaa69
Andres A <andres@netzstrategen.com>
2022-07-21 10:50:02 +0200

    fixup! Fixed missing package prefix/namespace in export patches command.

--- src/ExportPatchesCommand.php
+++ src/ExportPatchesCommand.php
@@ -28,8 +28,8 @@ class ExportPatchesCommand extends \WP_CLI_Command {
     }
 
     $dependencies = [];
-    foreach(array_keys($composer_json['require']) as $dependecy) {
-      $dependencies = $dependencies + [basename($dependecy) => $dependecy];
+    foreach (array_keys($composer_json['require']) as $dependecy) {
+      $dependencies[basename($dependecy)] = $dependecy;
     }
 
     if (!$dependencies) {
